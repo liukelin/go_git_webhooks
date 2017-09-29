f4814464619398bdd5cf275b8ef0026eb85b09fc
liu kelin <314566990@qq.com>
2017-09-29 16:17:10 +0800

    demo

--- service/server_cli.go
+++ service/server_cli.go
@@ -194,12 +194,12 @@ func loads_json(jsonStr string) (maps map[string]string) {
 /**
  * 执行shell命令
  */
-func run_shell(shell string) (out []byte, msg error) {
+func run_shell(shell string) (out string, msg error) {
 	cmd := exec.Command("/bin/sh", "-c", shell)
-	// err := cmd.Output()
-	out_, err := cmd.CombinedOutput()
+	out_, err := cmd.Output()
+	// out_, err := cmd.CombinedOutput()
 	if err != nil {
-		return out_, err
+		return string(out_), err
 		// panic(err.Error())
 	}
 	if err := cmd.Start(); err != nil {
@@ -209,5 +209,5 @@ func run_shell(shell string) (out []byte, msg error) {
 	if err := cmd.Wait(); err != nil {
 		// panic(err.Error())
 	}
-	return out_, err
+	return string(out_), err
 }
